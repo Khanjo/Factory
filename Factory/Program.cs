0b8f868c805b777e411ec51f31d735615c87373b
Joshua Khan <Khanjo@gmail.com>
2023-08-04 08:49:23 -0700

    Initialize project

---
+++ Factory/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Project.Models;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ProjectContext>(dbContextOptions => dbContextOptions.UseMySql(builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])));
+
+WebApplication app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles()
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.Run();
